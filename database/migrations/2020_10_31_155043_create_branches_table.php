01162a25d51630d49fe1b73e3ce18befd4c5fe06
obada <obadarbt5@hotmail.com>
2020-10-31 20:04:21 +0300

    first commit

---
+++ database/migrations/2020_10_31_155043_create_branches_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('cover')->nullable();
+            $table->datetime('open_time')->nullable();
+            $table->datetime('close_time')->nullable();
+            $table->enum('weekend',['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'])->nullable();
+            $table->bigInteger('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('Cities')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+}
